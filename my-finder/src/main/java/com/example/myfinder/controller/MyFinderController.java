40588f0db84b65f3908deb2491c5ee1daca1c985
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2023-01-31 15:57:59 +0100

    Création d'un controller
    accéder aux données exposées par "my-movie" dans le 2ème microservice "my-finder"

---
+++ my-finder/src/main/java/com/example/myfinder/controller/MyFinderController.java
@@ -0,0 +1,43 @@
+package com.example.myfinder.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/my-finder")
+public class MyFinderController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${my-movie.api}")
+    private String myMovieApi;
+
+    @GetMapping("/films")
+    public ResponseEntity<List<Film>> getAllFilms() {
+        try {
+            ResponseEntity<List<Film>> response = restTemplate.exchange(
+                    myMovieApi + "/films",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<Film>>() {}
+            );
+            return response;
+        } catch (Exception e) {
+            return handleFailure();
+        }
+    }
+
+    private ResponseEntity<List<Film>> handleFailure() {
+        // return fallback response or message indicating service is unavailable
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+    }
+}
+
