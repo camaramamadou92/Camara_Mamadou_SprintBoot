1bbad9956f9c9565db11f261714c36dc16fefae0
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2023-01-31 13:45:25 +0100

    getters et setters
    getters et setters pour liste de films

--- my-movie/src/main/java/com/example/mymovie/movie/Filmography.java
+++ my-movie/src/main/java/com/example/mymovie/movie/Filmography.java
@@ -7,4 +7,11 @@ public class Filmography {
     private List<Film> films;
 
     // getters and setters for films list
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
 }
