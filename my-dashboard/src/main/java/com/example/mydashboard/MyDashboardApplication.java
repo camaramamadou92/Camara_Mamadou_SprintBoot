62c318cab497fd9b146c009678bc0386089c8742
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2023-01-31 12:21:12 +0100

    Configurer Hystrix
    On ajoute les annotations @EnableHystrix et @EnableHystrixDashboard à notre classe principale.

--- my-dashboard/src/main/java/com/example/mydashboard/MyDashboardApplication.java
+++ my-dashboard/src/main/java/com/example/mydashboard/MyDashboardApplication.java
@@ -3,10 +3,14 @@ package com.example.mydashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableAdminServer
+@EnableHystrix
+@EnableHystrixDashboard
 
 
 public class MyDashboardApplication {
