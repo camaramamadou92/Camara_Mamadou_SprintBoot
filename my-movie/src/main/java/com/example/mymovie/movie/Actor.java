d2d26490add62126dbdf48ef9e359adb823b3267
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2023-01-31 13:49:51 +0100

    classe Actor
    ajout des getters et setters

--- my-movie/src/main/java/com/example/mymovie/movie/Actor.java
+++ my-movie/src/main/java/com/example/mymovie/movie/Actor.java
@@ -11,6 +11,43 @@ public class Actor {
     private Filmography filmography;
 
     // getters and setters for each field
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Filmography getFilmography() {
+        return filmography;
+    }
+
+    public void setFilmography(Filmography filmography) {
+        this.filmography = filmography;
+    }
+}
+
+
+
+
+
 }
 
 
