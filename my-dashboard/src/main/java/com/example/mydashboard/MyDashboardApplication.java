541aaf4c934f051c6e8b1df780134ffac2b744e6
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2023-01-31 12:17:39 +0100

    Configuration en tant que serveur Eureka
    on ajoute l’annotation @EnableEurekaServer à notre classe principale.

--- my-dashboard/src/main/java/com/example/mydashboard/MyDashboardApplication.java
+++ my-dashboard/src/main/java/com/example/mydashboard/MyDashboardApplication.java
@@ -2,8 +2,13 @@ package com.example.mydashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableAdminServer
+
+
 public class MyDashboardApplication {
 
 	public static void main(String[] args) {
