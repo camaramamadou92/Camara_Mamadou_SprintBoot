df239485795ababe8338ce5fca84077896f3289a
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2023-01-31 16:18:58 +0100

    création service acteur
    le service de récupération des acteurs

---
+++ my-movie/src/main/java/com/example/mymovie/service/ActorService.java
@@ -0,0 +1,29 @@
+package com.example.mymovie.service;
+
+import com.example.mymovie.movie.Actor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ActorService {
+    private final List<Actor> actors;
+
+    public ActorService(List<Actor> actors) {
+        this.actors = actors;
+    }
+
+    public List<Actor> getAllActors() {
+        return actors;
+    }
+
+    public List<Actor> getActorsByName(String name) {
+        return actors.stream()
+                .filter(actor -> actor.getName().equalsIgnoreCase(name))
+                .collect(Collectors.toList());
+    }
+
+    
+}
+
