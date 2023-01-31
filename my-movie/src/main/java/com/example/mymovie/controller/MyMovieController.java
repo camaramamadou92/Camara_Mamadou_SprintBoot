6d2a3e490795d3802ab5db732fc35339f547775d
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2023-01-31 15:20:01 +0100

    parametre MyMovieController
    On ajoute des descriptions pour chaque point de terminaison et paramètre de notre contrôleur à l’aide des annotations Swagger

--- my-movie/src/main/java/com/example/mymovie/controller/MyMovieController.java
+++ my-movie/src/main/java/com/example/mymovie/controller/MyMovieController.java
@@ -5,18 +5,25 @@ import java.util.stream.Collectors;
 
 import com.example.mymovie.movie.Actor;
 import com.example.mymovie.movie.Film;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "My Movie API", description = "API pour la gestion des films et des acteurs")
 public class MyMovieController {
 
     private List<Film> films;
     private List<Actor> actors;
 
+    @ApiOperation(value = "Obtenir tous les films ou filtrer par titre ou année de sortie")
     @GetMapping("/films")
-    public List<Film> getFilms(@RequestParam(required = false) String title,
+    public List<Film> getFilms(@ApiParam(value = "Filtrer par titre")
+            @RequestParam(required = false) String title,
+                               @ApiParam(value = "Filtrer par année de sortie")
                                @RequestParam(required = false) Integer releaseYear) {
         if (title != null) {
             return films.stream()
@@ -30,9 +37,10 @@ public class MyMovieController {
         }
         return films;
     }
-
+    @ApiOperation(value = "Obtenir tous les acteurs ou filtrer par nom")
     @GetMapping("/actors")
-    public List<Actor> getActors(@RequestParam(required = false) String name) {
+    public List<Actor> getActors(@ApiParam(value = "Filtrer par nom")
+            @RequestParam(required = false) String name) {
         if (name != null) {
             return actors.stream()
                     .filter(actor -> actor.getName().equals(name))
@@ -40,5 +48,6 @@ public class MyMovieController {
         }
         return actors;
     }
+
 }
 
